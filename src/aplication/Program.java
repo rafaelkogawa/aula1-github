fea3371d8a05a16744c90dca977fca98befa344e
Rafael kogawa <r_brum@hotmail.com>
2020-04-07 08:19:33 -0400

    adicionado pergunta do nome e mensagem de bom dia

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,19 @@
 package aplication;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
-	System.out.print("Olá mundo!");
-
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Olá, qual seu nome ? ");
+		String name = sc.nextLine();
+		if (name ==  " monica " ) {
+			System.out.println("Que nome lindo, Monica");
+		} else {
+			System.out.println("Tenha um bom dia " + name);
+		}
+sc.close();
 	}
 
 }
